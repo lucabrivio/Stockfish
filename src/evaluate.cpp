d793fa94ea9765c4d3b0558bd23ed09993bb7531
Luca Brivio <luca@brivio.name>
2014-01-27 06:33:54 +0100

    SPSA values after 43k iterations

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -774,7 +774,7 @@ Value do_evaluate(const Position& pos) {
         int rr = r * (r - 1);
 
         // Base bonus based on rank
-        Value mbonus = Value(14 * rr);
+        Value mbonus = Value(13 * rr);
         Value ebonus = Value(9 * (rr + r + 1));
 
         if (rr)
@@ -831,10 +831,10 @@ Value do_evaluate(const Position& pos) {
         // on the same rank and a bit smaller if it's on the previous rank.
         supportingPawns = pos.pieces(Us, PAWN) & adjacent_files_bb(file_of(s));
         if (supportingPawns & rank_bb(s))
-            ebonus += Value(r * 17);
+            ebonus += Value(r * 18);
 
         else if (supportingPawns & rank_bb(s - pawn_push(Us)))
-            ebonus += Value(r * 10);
+            ebonus += Value(r * 7);
 
         // Rook pawns are a special case: They are sometimes worse, and
         // sometimes better than other passed pawns. It is difficult to find
