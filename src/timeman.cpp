4ea13a37db1fd2dc260e2f8a7bfd79bae066f5a2
luca@brivio.name <luca@brivio.name>
2014-10-14 02:29:58 +0200

    fourth try, fixed

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -55,7 +55,7 @@ namespace {
     const double TStealRatio = (T == OptimumTime ? 0 : StealRatio);
 
     double thisMoveImportance = (move_importance(currentPly)
-      * (double)slowMover * (GameHistory.PonderHit ? .1 : .3) + .4 * GameHistory.LastBranchingFactor
+      * (double)slowMover * ((GameHistory.PonderHit ? .1 : .3) + .4 * GameHistory.LastBranchingFactor)
       ) / 100;
     double otherMovesImportance = 0;
 
