fdd9f32b4cd4329a81f5c75c8700730a5774c14e
luca@brivio.name <luca@brivio.name>
2014-12-31 00:42:43 +0100

    New linear version

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -42,7 +42,7 @@ namespace {
     double gamePhase = Search::RootPos.game_phase(), otherMoves = movesToGo - 1;
 
     double moveImportance = double(slowMover) / 100;
-    double otherMovesImportance = (320.0 - gamePhase) * otherMoves / 320.0 * exp(otherMoves / (gamePhase - 320.0));
+    double otherMovesImportance = otherMoves - (gamePhase * otherMoves / 320.0);
 
     double ratio1 = (TMaxRatio * moveImportance) / (TMaxRatio * moveImportance + otherMovesImportance);
     double ratio2 = (moveImportance + TStealRatio * otherMovesImportance) / (moveImportance + otherMovesImportance);
