de10ab9d0e5ba92cf276d8c29229a6eca8d53240
luca@brivio.name <luca@brivio.name>
2015-11-10 02:47:47 +0100

    More reasonable formula for consecutive easy moves.

--- src/timeman.h
+++ src/timeman.h
@@ -30,7 +30,7 @@
 class TimeManagement {
 public:
   void init(Search::LimitsType& limits, Color us, int ply);
-  void pv_instability(bool easy, int easyPlayed, double bestMoveChanges) { unstablePvFactor = !easy + bestMoveChanges + 0.08 * (1 + easyPlayed); }
+  void pv_instability(bool easy, int easyPlayed, double bestMoveChanges) { unstablePvFactor = 1.08 + bestMoveChanges - easy / (easyPlayed + 1); }
   int available() const { return int(optimumTime * unstablePvFactor * 0.72); }
   int maximum() const { return maximumTime; }
   int elapsed() const { return int(Search::Limits.npmsec ? Threads.nodes_searched() : now() - startTime); }
