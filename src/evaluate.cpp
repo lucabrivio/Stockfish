0472e43f125e5d6e720bd9e0cd13ea22508dc1bf
Luca Brivio <luca@brivio.name>
2016-09-03 20:26:57 +0200

    Take 2...
    Bench: 7104688

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -186,7 +186,6 @@ namespace {
   // Assorted bonuses and penalties used by evaluation
   const Score MinorBehindPawn     = S(16,  0);
   const Score BishopPawns         = S( 8, 12);
-  const Score BishopOpen          = S( 8,  0);
   const Score RookOnPawn          = S( 8, 24);
   const Score TrappedRook         = S(92,  0);
   const Score CloseEnemies        = S( 7,  0);
@@ -321,7 +320,7 @@ namespace {
             // depending on opponent's rook count
             if (Pt == BISHOP)
                 score -=  BishopPawns * ei.pi->pawns_on_same_color_squares(Us, s)
-                        + BishopOpen  * pos.count<ROOK>(~Us) * std::min(ei.pi->open_files(), 2);
+                        + make_score(3 * pos.count<ROOK>(~Us) * std::min(ei.pi->open_files(), 4), 0);
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
