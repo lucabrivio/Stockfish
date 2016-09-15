ad50b2e5df609bb7bb62792e0adefbda3779753c
Luca Brivio <luca@brivio.name>
2016-09-16 00:21:02 +0200

    Midgame bonus for levers which attack two pawns at the same time, decreasing as rank increases
    Original idea from S. Nicolet
    
    Bench: 5833572

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -49,8 +49,8 @@ namespace {
 
   // Lever bonus by rank
   const Score Lever[RANK_NB] = {
-    S( 0,  0), S( 0,  0), S(0, 0), S(0, 0),
-    S(17, 16), S(33, 32), S(0, 0), S(0, 0) };
+    S( 0,  0), S(-6,  0), S(-8, 0), S(-10, 0),
+    S( 5, 16), S(19, 32), S( 0, 0), S(  0, 0) };
 
   // Weakness of our pawn shelter in front of the king by [distance from edge][rank]
   const Value ShelterWeakness[][RANK_NB] = {
