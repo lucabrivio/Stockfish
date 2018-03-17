77719cd70050bb0fefb09a3ae50ba6754f38a358
Luca Brivio <luca@brivio.name>
2018-03-17 20:04:40 +0100

    Take 4.
    Bench: 5625163

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -867,7 +867,7 @@ namespace {
             + passed< WHITE>() - passed< BLACK>()
             + space<  WHITE>() - space<  BLACK>();
 
-    if (abs(eg_value(score)) > PawnValueEg / 32)
+    if (abs(eg_value(score)) > PawnValueEg / 64)
         score += initiative(eg_value(score));
 
     // Interpolate between a middlegame and a (scaled by 'sf') endgame score
