0cbbca758d6d594cd5cf7b3c6dd0d85469ccd08a
Luca Brivio <luca@brivio.name>
2018-03-17 21:03:06 +0100

    Take 2.
    Bench: 5431453

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -847,7 +847,7 @@ namespace {
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
     if (abs(v) > LazyThreshold)
-       return (pos.side_to_move() == WHITE ? v : -v) * 5 / 4;
+       return (pos.side_to_move() == WHITE ? v : -v) * 17 / 16;
 
     // Main evaluation begins here
 
